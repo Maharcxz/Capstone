aab88d272e371d4451ec98001b0ffb48bcd52fa9
Shiwohh <guzman.141265@globalcity.sti.edu.ph>
2025-09-28 21:07:59 +0800

    fixed the ar functionality fully.

--- JS/ProductLoader.js
+++ JS/ProductLoader.js
@@ -350,7 +350,7 @@ function openProductDetailModal(productId) {
             };
             
             const frameId = frameMapping[product.id] || 'glasses-6'; // Default frame
-            window.location.href = `ar-virtual-try-on.html?frame=${frameId}&productName=${encodeURIComponent(product.title)}`;
+            window.location.href = `try-on.html?frame=${frameId}&productName=${encodeURIComponent(product.title)}`;
         };
     }
 
